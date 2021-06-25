baa72459b655720103d9fcb371f4cc3182551220
ivan <uplay.ivan@gmail.com>
2021-06-25 22:39:41 +0200

    netem

--- backend/models/uzenetek.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const uzenetSchema = mongoose.Schema({
-  uzenete: { type: String, required: true },
-  nev: { type: String, required: true },
-});
-
-module.exports = mongoose.model("Uzenet", uzenetSchema);
